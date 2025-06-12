137e473cb5f69548212b0f6de8878de4d3a01308
Chandana Mallu <88961569+Chaaand03@users.noreply.github.com>
2025-06-12 15:50:43 -0400

    Updated index.js
    Meta pixels added.

--- pages/index.js
+++ pages/index.js
@@ -63,6 +63,24 @@ export default function Home() {
         <meta name="description" content="Master math and coding in 30 sessions. Book a free demo with expert tutors. Limited flash sale – 30-40% off! Trusted by 5,000+ students." />
         <meta name="keywords" content="math coding courses, free demo and flash sale, Online Courses, Learn UX, User Experience, Weekend UX, UX Certification" />
         <meta name="author" content="Book now,Limited offer" />
+
+        <!-- Meta Pixel Code -->
+            <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1251646379953168');
+            fbq('track', 'PageView');
+            </script>
+            <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1251646379953168&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
       </Head>
 
       {/* Hero Section */}
